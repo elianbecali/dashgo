af8b381bd3ca8f7317bacdfbacbc0a7fc108b5b0
Elian Becali <elianbecaliaguiar@gmail.com>
2021-05-28 19:59:31 -0400

    refactor(sidebar): create nav section component

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,48 @@
+import { Box, Icon, Link, Stack, Text } from '@chakra-ui/react';
+import {
+  RiContactsLine,
+  RiDashboardLine,
+  RiGitMergeLine,
+  RiInputMethodLine
+} from 'react-icons/ri';
+import { NavSection } from './NavSection';
+
+export function Sidebar() {
+  return (
+    <Box as="aside" w="64" mr="8">
+      <Stack spacing="12" align="flex-start">
+        <NavSection title="GERAL">
+          <Link display="flex" align="center">
+            <Icon as={RiDashboardLine} fontSize="20" />
+            <Text ml="4" fontWeight="medium">
+              Dashboard
+            </Text>
+          </Link>
+
+          <Link display="flex" align="center">
+            <Icon as={RiContactsLine} fontSize="20" />
+            <Text ml="4" fontWeight="medium">
+              Usuários
+            </Text>
+          </Link>
+        </NavSection>
+
+        <NavSection title="AUTOMAÇÃO">
+          <Link display="flex" align="center">
+            <Icon as={RiInputMethodLine} fontSize="20" />
+            <Text ml="4" fontWeight="medium">
+              Formulários
+            </Text>
+          </Link>
+
+          <Link display="flex" align="center">
+            <Icon as={RiGitMergeLine} fontSize="20" />
+            <Text ml="4" fontWeight="medium">
+              Automação
+            </Text>
+          </Link>
+        </NavSection>
+      </Stack>
+    </Box>
+  );
+}
